88942466bc9a04ec0d4aa4b05803ec948ba7fb86
zsp-pc <zsp-pc@zsp>
2015-07-08 17:39:50 +0800

    gluonj

--- src/stone/StoneException.java
+++ src/stone/StoneException.java
@@ -1,8 +1,14 @@
 package stone;
 
+import stone.ast.ASTree;
+
 public class StoneException extends RuntimeException {
 	
 	private static final long serialVersionUID = 1L;
 
 	public StoneException(String m) { super(m); }
+	
+	public StoneException(String m, ASTree t) {
+        super(m + " " + t.location());
+    }
 }
